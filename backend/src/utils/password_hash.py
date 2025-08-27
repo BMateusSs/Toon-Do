289027b01a00b47e720904decfe22c650c9a953b
BMateusSs <bmdss0810@gmail.com>
2025-08-27 18:17:53 -0300

    Incluindo validação de senha para o login

--- backend/src/utils/password_hash.py
+++ backend/src/utils/password_hash.py
@@ -9,3 +9,19 @@ def password_hash(password):
 
     return hashed_password
 
+def check_login(password_login, password_db):
+
+    password_login_byte = password_login.encode('utf-8')
+    
+    if isinstance(password_db, str):
+        password_db_byte = password_db.encode('utf-8')
+    else:
+        password_db_byte = password_db
+        
+    if bcrypt.checkpw(password_login_byte, password_db_byte):
+        print("Login bem-sucedido!")
+        return True
+    else:
+        print("Senha incorreta.")
+        return False
+    
