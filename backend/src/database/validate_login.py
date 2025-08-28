1590a5ac545514db072c65509201041b927faa61
BMateusSs <bmdss0810@gmail.com>
2025-08-28 11:32:27 -0300

    Fechar cursor e connexão com o banco

--- backend/src/database/validate_login.py
+++ backend/src/database/validate_login.py
@@ -5,28 +5,31 @@ def validate_login(credential, password):
     conn = connection()
     cursor = conn.cursor()
 
-    query = '''
-    SELECT
-        id,
-        password_hash
-    FROM 
-        users
-    WHERE
-        username = %s OR email = %s
-    '''
-    cursor.execute(query, (credential, credential))
-    response = cursor.fetchone()
+    try:
+        query = '''
+        SELECT
+            id,
+            password_hash
+        FROM 
+            users
+        WHERE
+            username = %s OR email = %s
+        '''
+        cursor.execute(query, (credential, credential))
+        response = cursor.fetchone()
 
-    if response is None:
-        return False, 'Usuário/e-mail não encontrado.'
-    
-    user_id = response[0]
-    password_db = response[1]
-    isValidPassword = check_login(password, password_db)
+        if response is None:
+            return False, 'Usuário/e-mail não encontrado.'
+        
+        user_id = response[0]
+        password_db = response[1]
+        isValidPassword = check_login(password, password_db)
 
-    if isValidPassword:
-        return user_id
-
+        if isValidPassword:
+            return True, user_id
+    finally:
+        cursor.close()
+        conn.close()
+        
     return False, 'Senha incorreta.'
 
-validate_login('bmateus', 'm0ck1ngj4y')
