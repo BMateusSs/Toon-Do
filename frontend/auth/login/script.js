12a14cd4dc04a9d76034dadab2f71308e0f75df1
BMateusSs <bmdss0810@gmail.com>
2025-08-28 11:35:59 -0300

    Corrigindo script

--- frontend/auth/login/script.js
+++ frontend/auth/login/script.js
@@ -1,28 +1,32 @@
-import {useFetch} from '../../utils/useFetch'
+import {useFetch} from '../../utils/useFetch.js'
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log("JS carregado!");
+
     const loginForm = document.getElementById('loginForm');
-    const errorMessage = document.getElementById('error-meaage');
+    const errorMessage = document.getElementById('erro-message'); // corrigido
     const login = document.getElementById('email-user');
-    const password = document.getElementById('password')
+    const password = document.getElementById('password');
 
     loginForm.addEventListener('submit', async (event) => {
-        event.preventDefault()
+        event.preventDefault(); // agora vai funcionar
 
         const url = 'http://127.0.0.1:5000/auth/login';
 
-        body = {
+        const body = {
             credential: login.value,
             password: password.value
-        }
+        };
 
         const {result, error} = await useFetch(url, body);
 
-        if (error){
-            errorMessage.innerHTML=error
-        }else {
-            const token = result.token
+        if (error) {
+            errorMessage.innerHTML = error;
+        } else {
+            const token = result.token;
+            alert(token);
             localStorage.setItem('token', token);
         }
-    })
-})
+    });
+});
