8e4d8a831fdb06782ce834942bf2f252d41682df
BMateusSs <bmdss0810@gmail.com>
2025-08-29 18:39:19 -0300

    Consultar tarefas com data limite aproximada

--- backend/src/database/selections/tasks/home.py
+++ backend/src/database/selections/tasks/home.py
@@ -48,4 +48,44 @@ def projects(user_id):
         cursor.close()
         conn.close()
 
-print(projects(1))
+def tasks_today(user_id):
+    conn = connection()
+    cursor = conn.cursor()
+
+    try:
+        query = '''
+        SELECT
+            t.id AS task_id,
+            t.title,
+            t.description_task,
+            t.color AS task_color,
+            t.limit_date,
+            p.id AS proj_id,
+            p.color AS proj_color
+        FROM
+            projects p
+        INNER JOIN
+            tasks t ON p.id = t.proj_id
+        WHERE
+            p.user_id = %s AND t.limit_date = CURDATE()
+        LIMIT 5;
+        '''
+        cursor.execute(query, (user_id,))
+        tasks = cursor.fetchall()
+
+        response = []
+        for task in tasks:
+            response.append({
+                'task_id': task[0],
+                'title': task[1],
+                'description': task[2],
+                'task_color': task[3],
+                'limit_date': task[4],
+                'proj_id': task[5],
+                'proj_color': task[6]
+            })
+        return response
+    
+    except:
+        cursor.close()
+        conn.close()
