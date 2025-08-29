6728e802b4f72dd89c99b71319242c732e86022f
BMateusSs <bmdss0810@gmail.com>
2025-08-29 18:44:27 -0300

    Acrescentando função para tarefas diárias na hota home

--- backend/src/app/tasks/routes.py
+++ backend/src/app/tasks/routes.py
@@ -1,17 +1,19 @@
 from . import tasks_bp
 from flask import request, jsonify
 from backend.src.utils.token_required import token_required
-from backend.src.database.selections.tasks.home import total_projects, projects
+from backend.src.database.selections.tasks.home import total_projects, projects, tasks_today
 
 @tasks_bp.route('/home', methods=['GET'])
 @token_required
 def home(user_id):
     total_proj = total_projects(user_id)
     recent_projects = projects(user_id)
+    tasks = tasks_today(user_id)
     
     response = {
         'total_projects': total_proj,
-        'recent_projects': recent_projects
+        'recent_projects': recent_projects,
+        'tasks_today': tasks
     }
 
     return jsonify(response)
