0243be8f251258aba6a74d468915f52dde2aa5ca
BMateusSs <bmdss0810@gmail.com>
2025-09-02 12:40:01 -0300

    Função para renderizar a home dinamicamente

--- frontend/home/renderHome.js
+++ frontend/home/renderHome.js
@@ -1,8 +1,18 @@
+import { useFetch } from '../utils/useFetch.js'
+import { methodGet } from '../utils/methods.js'
 import { renderHabits } from './renderHabits.js'
 import { renderRecentProjects } from './renderRecentProjects.js'
 
-export function renderHome(){
-    renderHabits()
-    // Temporariamente reutilizando renderProjects; idealmente criar renderRecentProjects
-    renderRecentProjects()
+export async function renderHome(){
+    const url = 'http://127.0.0.1:5000/tasks/home'
+    const config = methodGet()
+
+    const {result, error} = await useFetch(url, config)
+
+    const recentProjects = result.recent_projects;
+    const tasksToday = result.tasks_today;
+    const todayHabits = result.habits_today;
+
+    renderHabits(todayHabits)
+    renderRecentProjects(recentProjects)
 }
