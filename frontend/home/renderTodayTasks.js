f6b01048148bbbfc2861385b1c42eece850772df
BMateusSs <bmdss0810@gmail.com>
2025-09-02 13:14:40 -0300

    Função para renderizar tarefas na home

---
+++ frontend/home/renderTodayTasks.js
@@ -0,0 +1,32 @@
+export function renderTodayTasks(tasksToday){
+    
+    const container = document.querySelector('.tasks-today');
+    container.innerHTML=``
+    if (tasksToday.length === 0){
+        container.innerHTML = `
+            <div class="empty-container tasks barlow-bold">
+                Sem tarefas para hoje. Adicionar +
+            </div>
+        `;
+    } else {
+        tasksToday.forEach((task) => {
+            const card = document.createElement('div');
+            const date = formatDate(task.limit_date)
+            card.innerHTML = `
+            <div class="date-task">
+                <p class="barlow-regular">${date}</p>
+            </div>
+            <div class="task-title">
+                <p class="barlow-bold">${task.title}</p>
+            </div>
+            <div>
+                <p class="barlow-regular">${task.description}</p>
+            </div>
+            `;
+            
+            card.classList.add('task-card')
+            card.style.borderColor = `#${task.proj_color}`
+            container.appendChild(card);
+        });
+}
+}
